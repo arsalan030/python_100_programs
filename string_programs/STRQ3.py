1fb7a6baece11ab0075a8d3e7db0b094383ea29d
arsalangithub <ahmedarsalan030@gmail.com>
2023-04-19 03:22:26 +0500

    python programming string question

---
+++ string_programs/STRQ3.py
@@ -0,0 +1,28 @@
+# revers words in given string in python
+strings = "arsalan is a boy"
+st = []
+for char in strings[::-1]:
+    st.append(char)
+a = "".join(st)
+
+print(a)
+
+# another way to this question
+# Function to reverse words of string
+
+def rev_sentence(sentence):
+
+	# first split the string into words
+	words = sentence.split(' ')
+
+	# then reverse the split string list and join using space
+	reverse_sentence = ' '.join(reversed(words))
+
+	# finally return the joined string
+	return reverse_sentence
+
+input = 'geeks quiz practice code'
+print (rev_sentence(input))
+
+
+
