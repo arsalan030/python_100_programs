0fb87fbe35cfbb7cd8e84c76962b8f0189f4511f
arsalangithub <ahmedarsalan030@gmail.com>
2023-04-19 16:25:32 +0500

    python programming 100 question

---
+++ string_programs/STRQ7.py
@@ -0,0 +1,30 @@
+# upper case half string
+string = " arsalan ahmed is a boy"
+half = len(string)//2
+string = string [:half] + string[half:].upper()
+print(string)
+
+# Python3 code to demonstrate working of
+# Uppercase Half String
+# Using upper() + loop + len()
+
+# initializing string
+test_str = 'geeksforgeeks'
+
+# printing original string
+print("The original string is : " + str(test_str))
+
+# computing half index
+hlf_idx = len(test_str) // 2
+
+res = ''
+for idx in range(len(test_str)):
+
+	# uppercasing later half
+	if idx >= hlf_idx:
+		res += test_str[idx].upper()
+	else:
+		res += test_str[idx]
+
+# printing result
+print("The resultant string : " + str(res))
